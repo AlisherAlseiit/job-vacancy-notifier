73a50cc9ef1533aae65de2244ba08fc910c8c8ef
Alish <magawin51@gmail.com>
2023-05-03 17:57:07 +0600

    changed config attrs

--- app/config.py
+++ app/config.py
@@ -11,6 +11,7 @@ class TwilioSettings(BaseSettings):
     database_password: str
     database_name: str
     database_username: str
+    database_instance_connection_name: str
 
     class Config:
         env_file = ".env"
